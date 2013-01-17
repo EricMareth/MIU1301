7fd379d972d13e9e14aebc3b77df9adb7cc01dfd
EricMareth <EricMareth@fullsail.edu>
2013-01-16 19:59:54 -0500

    gold and bronze alike added

---
+++ gold/js/json.js
@@ -0,0 +1,246 @@
+// Mobile App
+// Eric Mareth
+// Mobile Interfaces and Usability 1301
+
+
+var json = {
+	"character1" : {
+		"name" :	["Name:", "Fuzzi"],
+		"story":	["Story:", "Fuzzi's Day Out"],
+		"land":		["Land:", "FuzziLand"],
+		"town":		["Town:", "FuzzCentral"],
+		"gender":	["Sex:", "It's Complicated"],
+		"age":		["Age:", "150"],
+		"type":		["Character Type:", "Hero"],
+		"details":	["Details:", "Our very fuzzy hero."],
+		"created":	["Birthdate:", "2012-11-12"]
+	},
+	
+	"character2" : {
+		"name" :	["Name:", "Danala"],
+		"story":	["Story:", "The Witch Queen"],
+		"land":		["Land:", "Etera"],
+		"town":		["Town:", "Cabra"],
+		"gender":	["Sex:", "Female"],
+		"age":		["Age:", "35"],
+		"type":		["Character Type:", "Villain"],
+		"details":	["Details:", "The wickedest queen in all of Etera."],
+		"created":	["Birthdate:", "2004-06-25"]
+	},
+	
+	"character3" : {
+		"name" :	["Name:", "The Creeper"],
+		"story":	["Story:", "HorriDreams"],
+		"land":		["Land:", "???"],
+		"town":		["Town:", ""],
+		"gender":	["Sex:", "Male"],
+		"age":		["Age:", "60"],
+		"type":		["Character Type:", "Supporting"],
+		"details":	["Details:", "The host of the HorriDreams comic series."],
+		"created":	["Birthdate:", "2011-06-11"]
+	},
+	
+	"character4" : {
+		"name" :	["Name:", "Jacob"],
+		"story":	["Story:", "The Witch Queen"],
+		"land":		["Land:", "Etera"],
+		"town":		["Town:", "Cabra"],
+		"gender":	["Sex:", "Male"],
+		"age":		["Age:", "20"],
+		"type":		["Character Type:", "Henchman"],
+		"details":	["Details:", "Danala's Son."],
+		"created":	["Birthdate:", "2004-06-25"]
+	},
+	
+	"character5" : {
+		"name" :	["Name:", "Fussy"],
+		"story":	["Story:", "Fuzzi's Day Out"],
+		"land":		["Land:", "FuzziLand"],
+		"town":		["Town:", "FuzzCentral"],
+		"gender":	["Sex:", "Female"],
+		"age":		["Age:", "170"],
+		"type":		["Character Type:", "Supporting"],
+		"details":	["Details:", "Fuzzi's mother."],
+		"created":	["Birthdate:", "2012-11-12"]
+	},
+	
+	"character6" : {
+		"name" :	["Name:", "Velo"],
+		"story":	["Story:", "The Witch Queen"],
+		"land":		["Land:", "Etera"],
+		"town":		["Town:", "Cliff Village"],
+		"gender":	["Sex:", "Male"],
+		"age":		["Age:", "200"],
+		"type":		["Character Type:", "Mentor"],
+		"details":	["Details:", "Peaceful dragon who saved and raised Danala as his own child."],
+		"created":	["Birthdate:", "2004-06-25"]
+	},
+	
+	"character7" : {
+		"name" :	["Name:", "Grumble"],
+		"story":	["Story:", "Fuzzi's Day Out"],
+		"land":		["Land:", "FuzziLand"],
+		"town":		["Town:", "Hard Place"],
+		"gender":	["Sex:", "Male"],
+		"age":		["Age:", "180"],
+		"type":		["Character Type:", "Villain"],
+		"details":	["Details:", "Hermit who plans ways to disrupt the Fuzzi people when they are having fun."],
+		"created":	["Birthdate:", "2012-11-12"]
+	},
+
+	"character8" : {
+		"name" :	["Name:", "Samuel"],
+		"story":	["Story:", "The Witch Queen"],
+		"land":		["Land:", "Etera"],
+		"town":		["Town:", "Magic Village"],
+		"gender":	["Sex:", "Male"],
+		"age":		["Age:", "20"],
+		"type":		["Character Type:", "Love-Interest"],
+		"details":	["Details:", "Danala's only love, and Jacob's father. Accidentally killed by Thurne, the Hunter Dragon."],
+		"created":	["Birthdate:", "2004-06-25"]
+	},
+
+	"character9" : {
+		"name" :	["Name:", "Wilbur"],
+		"story":	["Story:", "The Smell of Fear"],
+		"land":		["Land:", "Horridreams Story"],
+		"town":		["Town:", ""],
+		"gender":	["Sex:", "Male"],
+		"age":		["Age:", "40"],
+		"type":		["Character Type:", "Hero"],
+		"details":	["Details:", "Wilbur has nightmares that prevent him from using the camp shower."],
+		"created":	["Birthdate:", "2004-06-25"]
+	},
+	
+	"character10" : {
+		"name" :	["Name:", "Plinus"],
+		"story":	["Story:", "The Witch Queen"],
+		"land":		["Land:", "Etera"],
+		"town":		["Town:", "Port Albra"],
+		"gender":	["Sex:", "Male"],
+		"age":		["Age:", "45"],
+		"type":		["Character Type:", "Off-Screen"],
+		"details":	["Details:", "The recorder of all events within the land of Etera."],
+		"created":	["Birthdate:", "2004-06-25"]
+	},
+
+	"character11" : {
+		"name" :	["Name:", "Alia"],
+		"story":	["Story:", "The Witch Queen"],
+		"land":		["Land:", "Etera"],
+		"town":		["Town:", "Albra"],
+		"gender":	["Sex:", "Female"],
+		"age":		["Age:", "20"],
+		"type":		["Character Type:", "Hero"],
+		"details":	["Details:", "Hero of the tale, figures out how to stop the witch queen forever."],
+		"created":	["Birthdate:", "2004-06-25"]
+	},
+
+	"character12" : {
+		"name" :	["Name:", "Bren"],
+		"story":	["Story:", "The Witch Queen"],
+		"land":		["Land:", "Etera"],
+		"town":		["Town:", "Albra Forest"],
+		"gender":	["Sex:", "Male"],
+		"age":		["Age:", "25"],
+		"type":		["Character Type:", "Love-Interest"],
+		"details":	["Details:", "Apprentice magician and Alia's love interest."],
+		"created":	["Birthdate:", "2004-06-25"]
+	},
+
+	"character13" : {
+		"name" :	["Name:", "Bucky"],
+		"story":	["Story:", "Misfit Monsters"],
+		"land":		["Land:", "United States"],
+		"town":		["Town:", "Ravenvale"],
+		"gender":	["Sex:", "Male"],
+		"age":		["Age:", "15"],
+		"type":		["Character Type:", "Hero"],
+		"details":	["Details:", "Unfortunate buck-toothed vampire teen."],
+		"created":	["Birthdate:", "2010-09-20"]
+	},
+
+	"character14" : {
+		"name" :	["Name:", "Millie"],
+		"story":	["Story:", "Misfit Monsters"],
+		"land":		["Land:", "United States"],
+		"town":		["Town:", "Ravenvale"],
+		"gender":	["Sex:", "Female"],
+		"age":		["Age:", "15"],
+		"type":		["Character Type:", "Hero"],
+		"details":	["Details:", "Student witch who has trouble casting spells because of her lisp."],
+		"created":	["Birthdate:", "2010-09-20"]
+	},
+
+	"character15" : {
+		"name" :	["Name:", "Harry"],
+		"story":	["Story:", "Misfit Monsters"],
+		"land":		["Land:", "United States"],
+		"town":		["Town:", "Ravenvale"],
+		"gender":	["Sex:", "Male"],
+		"age":		["Age:", "15"],
+		"type":		["Character Type:", "Hero"],
+		"details":	["Details:", "Hairless werewolf - goes through the change, but has no hair."],
+		"created":	["Birthdate:", "2010-09-20"]
+	},
+
+	"character16" : {
+		"name" :	["Name:", "Nestor"],
+		"story":	["Story:", "Misfit Monsters"],
+		"land":		["Land:", "United States"],
+		"town":		["Town:", "Ravenvale"],
+		"gender":	["Sex:", "Male"],
+		"age":		["Age:", "15"],
+		"type":		["Character Type:", "Hero"],
+		"details":	["Details:", "Sea creature with a fear of deep water."],
+		"created":	["Birthdate:", "2010-09-20"]
+	},
+
+	"character17" : {
+		"name" :	["Name:", "Mister E"],
+		"story":	["Story:", "Misfit Monsters"],
+		"land":		["Land:", "United States"],
+		"town":		["Town:", "Ravenvale"],
+		"gender":	["Sex:", "Male"],
+		"age":		["Age:", "30"],
+		"type":		["Character Type:", "Mentor"],
+		"details":	["Details:", "The childrens' teacher - an invisible man who is not completely invisible."],
+		"created":	["Birthdate:", "2010-09-20"]
+	},
+
+	"character18" : {
+		"name" :	["Name:", "Thurne"],
+		"story":	["Story:", "The Witch Queen"],
+		"land":		["Land:", "Etera"],
+		"town":		["Town:", "The Dragonlands"],
+		"gender":	["Sex:", "Male"],
+		"age":		["Age:", "200"],
+		"type":		["Character Type:", "Supporting"],
+		"details":	["Details:", "Dragon on the hunt for Danala because he mistakingly believes she killed Velo in exchange for eternal life."],
+		"created":	["Birthdate:", "2004-06-25"]
+	},
+
+	"character19" : {
+		"name" :	["Name:", "Veruha"],
+		"story":	["Story:", "The Witch Queen"],
+		"land":		["Land:", "Etera"],
+		"town":		["Town:", "Witch Island"],
+		"gender":	["Sex:", "Female"],
+		"age":		["Age:", "40"],
+		"type":		["Character Type:", "Supporting"],
+		"details":	["Details:", "Witch queen that Danala challenged and killed for wanting to sacrifice her son, Jacob."],
+		"created":	["Birthdate:", "2004-06-25"]
+	},
+
+	"character20" : {
+		"name" :	["Name:", "Vir"],
+		"story":	["Story:", "The Witch Queen"],
+		"land":		["Land:", "Etera"],
+		"town":		["Town:", "Cabra Wood"],
+		"gender":	["Sex:", "Male"],
+		"age":		["Age:", "50"],
+		"type":		["Character Type:", "Walk-On"],
+		"details":	["Details:", "Leader of the horde that slaughtered the people of Cliff Village and left Danala an orphan, and who killed the peaceful dragon Velo."],
+		"created":	["Birthdate:", "2004-06-25"]
+	},
+}
